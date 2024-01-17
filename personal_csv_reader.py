a8bbb045a6d35734ffb28c0d14249d1d8cff38fb
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2024-01-17 15:00:57 +1000

    Add skeleton functions in personal_csv_reader.py

---
+++ personal_csv_reader.py
@@ -0,0 +1,33 @@
+"""
+
+A simple Python script that reads from a CSV transaction export and writes to a file.
+
+By: Joshua Delos Santos
+
+Date started: 17/01/2024
+
+"""
+
+
+def main():
+    pass
+
+
+def load_data():
+    pass
+
+
+def save_data():
+    pass
+
+
+def sort_data():
+    pass
+
+
+def display_data():
+    pass
+
+
+def get_valid_filename():
+    pass
