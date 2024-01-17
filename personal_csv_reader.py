31d308a68c08faf9fdbea9478e4831f659a06d04
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2024-01-17 15:22:55 +1000

    Add code for load_data function in personal_csv_reader.py

--- personal_csv_reader.py
+++ personal_csv_reader.py
@@ -10,11 +10,23 @@ Date started: 17/01/2024
 
 
 def main():
-    pass
+    filename = input("CSV filename: ")
+    data = filename
+    load_data(filename)
 
 
-def load_data():
-    pass
+def load_data(filename):
+    data = []
+    with open(filename, 'r') as in_file:
+        in_file.readline()  # Skip CSV header
+        lines = in_file.readlines()
+
+    for line in lines:
+        parts = line.split(',')
+        data.append([parts[1], parts[2], parts[3], parts[4], parts[5]])
+        # Example:  Date, Description, Debit Amount, credit Amount, Balance
+
+    return data
 
 
 def save_data():
@@ -31,3 +43,7 @@ def display_data():
 
 def get_valid_filename():
     pass
+
+
+if __name__ == '__main__':
+    main()
